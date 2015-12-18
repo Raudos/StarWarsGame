ac766719bb48a014b87a7083c9c67c99ed98adbe
Piotr Sklepiński <Piotr Sklepiski>
2015-12-18 12:18:31 +0100

    Added left squadron with all their functions.

--- js/script.js
+++ js/script.js
@@ -44,6 +44,8 @@ var tieX = 120;
 var tieY = 0;
 var tieWidth = 24;
 var tieHeight = 24;
+var tieLeftWidth = 28;
+var tieLeftHeight = 28;
 
 // Images
 var xWing = new Image();
@@ -63,6 +65,9 @@ playerLife.src = 'css/images/r2d2.png';
 
 var warning = new Image();
 warning.src = 'css/images/warning.png';
+
+var tieLeft = new Image();
+tieLeft.src = 'css/images/tief_left.png';
 
 
 // Event listeners
@@ -91,7 +96,7 @@ function keyDownHandler(e) {
 function keyUpHandler(e) {
     if(e.keyCode == 39) {
         rightPressed = false;
-    }
+    }	
     else if(e.keyCode == 37) {
         leftPressed = false;
     }
@@ -154,63 +159,133 @@ function drawBg() {
 }
 
 // Tie Fighter Functions.
-tieSquadron = [];
-function drawTie() {
-	var n = tieSquadron.length;
-	for (var i = 0; n > i; i++) {
-		if (tieSquadron[i][2] == 1) {
 
-			ctx.drawImage(tie, tieSquadron[i][0], tieSquadron[i][1], tieWidth, tieHeight);
+// Left Squadron 
+tieLeftSquadron = [];
+function drawLeftTie() {
+	var n = tieLeftSquadron.length;
+	for (var i = 0; n > i; i++) {
+		if (tieLeftSquadron[i][2] == 1) {
+			ctx.drawImage(tieLeft, tieLeftSquadron[i][0], tieLeftSquadron[i][1], tieLeftWidth, tieLeftHeight);
+			tieLeftSquadron[i][1] += 1;
+			tieLeftSquadron[i][0] += 1;
 		}
 	}
 }
-function addTie() {
+function addLeftTie() {
 	var n = canvas.width / 5;
-	var tie = tieSquadron.length; // Only 5 ties please!
+	var tie = tieLeftSquadron.length; // Only 5 ties please!
 	
 	if (tie < 5) {
 		for (var i = 1; 5 >= i; i++) {
-			var t = [n * i - tieWidth * 1.5, 0, 1]; // 1.5 is to center it a bit, delete later on
-			tieSquadron.push(t);
+			var t = [- tieLeftWidth * i * 2, - 100 - tieLeftHeight * i, 1, 1]; // Magical numbers to adjust it a bit | x, y, status, type
+			tieLeftSquadron.push(t);
 		}
 	}
 }
 
-tieLasers = [];
-var shotTCD = 40;
-function addTLaser() {
-	var n = tieSquadron.length;
-	if (shotTCD >= 40)
+tieLeftLasers = [];
+var shotLeftTCD = 40;
+function addLeftTLaser() {
+	var n = tieLeftSquadron.length;
+	if (shotLeftTCD >= 40)
 	for (var i = 0; n > i; i++) {
-		if (tieSquadron[i][2] == 1) {
-			var tLaserX = tieSquadron[i][0] + tieWidth / 2;
-			var tLaserY = tieSquadron[i][1] + tieHeight / 2;
+		if (tieLeftSquadron[i][2] == 1) {
+			var tLaserX = tieLeftSquadron[i][0] + tieLeftWidth / 2;
+			var tLaserY = tieLeftSquadron[i][1] + tieLeftHeight / 2;
 			var tLaserStatus = 1;
 			var tLaserArray = [tLaserX, tLaserY, tLaserStatus];
-			tieLasers.push(tLaserArray);
-			shotTCD = 0; // Shot reset
+			tieLeftLasers.push(tLaserArray);
+			shotLeftTCD = 0; // Shot reset
 		}
 	}
 }
 
-function moveTLaser() {
-	var n = tieLasers.length;
+function moveLeftTLaser() {
+	var n = tieLeftLasers.length;
 	for (var i = 0; n > i; i++) {
-		tieLasers[i][1] += 2;
+		tieLeftLasers[i][1] += 2;
+		tieLeftLasers[i][0] += 2;
 	}
 }
 
-function drawTLaser() {
-	var n = tieLasers.length;
+function drawLeftTLaser() {
+	var n = tieLeftLasers.length;
 	for (var i = 0; n > i; i++) {
-		if (tieLasers[i][1] < canvas.height) {
+		if (tieLeftLasers[i][1] < canvas.height) {
 			ctx.beginPath();
-			ctx.rect(tieLasers[i][0], tieLasers[i][1], 1, 4);
+			ctx.rect(tieLeftLasers[i][0], tieLeftLasers[i][1], 1, 2);
 			ctx.fillStyle = "rgb(86,206,82)";
 			ctx.fill();
 			ctx.closePath();
 		} else {
-			tieLasers.splice(i, 1);
+			tieLeftLasers.splice(i, 1);
+		}
+	}
+}
+
+
+// Middle Squadron
+tieMidSquadron = [];
+function drawMidTie() {
+	var n = tieMidSquadron.length;
+	for (var i = 0; n > i; i++) {
+		tieMidSquadron[i][1] += 1; //Before drawing so you they actually end up being deleted
+		if (tieMidSquadron[i][2] == 1) {
+			ctx.drawImage(tie, tieMidSquadron[i][0], tieMidSquadron[i][1], tieWidth, tieHeight);
+		}
+		if (tieMidSquadron[i][1] >= canvas.height) {
+			tieMidSquadron.splice(i, 1);
+		}
+	}
+}
+function addMidTie() {
+	var n = canvas.width / 5;
+	var tie = tieMidSquadron.length; // Only 5 ties please!
+	
+	if (tie < 5) {
+		for (var i = 1; 5 >= i; i++) {
+			var t = [n * i - tieWidth * 1.5, -tieHeight, 1, 2]; // 1.5 is to center it a bit, delete later on | x, y, status, type
+			tieMidSquadron.push(t);
+		}
+	}
+}
+
+tieMidLasers = [];
+var shotMidTCD = 40;
+function addMidTLaser() {
+	var n = tieMidSquadron.length;
+	if (shotMidTCD >= 40)
+	for (var i = 0; n > i; i++) {
+		if (tieMidSquadron[i][2] == 1) {
+			var tLaserX = tieMidSquadron[i][0] + tieWidth / 2;
+			var tLaserY = tieMidSquadron[i][1] + tieHeight / 2;
+			var tLaserStatus = 1;
+			var tLaserArray = [tLaserX, tLaserY, tLaserStatus];
+			tieMidLasers.push(tLaserArray);
+			shotMidTCD = 0; // Shot reset
+		}
+	}
+}
+
+function moveMidTLaser() {
+	var n = tieMidLasers.length;
+	for (var i = 0; n > i; i++) {
+		tieMidLasers[i][1] += 2;
+	}
+}
+
+function drawMidTLaser() {
+	var n = tieMidLasers.length;
+	for (var i = 0; n > i; i++) {
+		if (tieMidLasers[i][1] < canvas.height) {
+			ctx.beginPath();
+			ctx.rect(tieMidLasers[i][0], tieMidLasers[i][1], 1, 4);
+			ctx.fillStyle = "rgb(86,206,82)";
+			ctx.fill();
+			ctx.closePath();
+		} else {
+			tieMidLasers.splice(i, 1);
 		}
 	}
 }
@@ -267,14 +342,30 @@ function drawLaser() {
 		}
 	}
 }
-function laserCol() {
+// XWing's lasers colision with mid ties
+function laserMidCol() {
 	var n = lasersXwing.length;
-	var tie = tieSquadron.length;
-	for (var i = 0; n > i; i++) { // Check each laser shot
+	var tie = tieMidSquadron.length;
+	for (var i = 0; n > i; i++) { // Check each mid tie
 		for (var j = 0; tie > j; j++) {
-			if (lasersXwing[i][1] <= tieSquadron[j][1] + tieHeight) {
-				if (lasersXwing[i][0] < tieSquadron[j][0] + tieWidth && lasersXwing[i][0] + 2 > tieSquadron[j][0]) {
-					tieSquadron[j][2] = 0;
+			if (lasersXwing[i][1] <= tieMidSquadron[j][1] + tieHeight && tieMidSquadron[j][2] == 1) {
+				if (lasersXwing[i][0] < tieMidSquadron[j][0] + tieWidth && lasersXwing[i][0] + 2 > tieMidSquadron[j][0]) {
+					tieMidSquadron[j][2] = 0;
+					lasersXwing.splice(i, 1);
+				}
+			}
+		}
+	}
+}
+// Xwing's lasers colision with left ties
+function laserLeftCol() {
+	var n = lasersXwing.length;
+	var tie = tieLeftSquadron.length;
+	for (var i = 0; n > i; i++) { // Check each mid tie
+		for (var j = 0; tie > j; j++) {
+			if (lasersXwing[i][1] <= tieLeftSquadron[j][1] + tieHeight && tieLeftSquadron[j][2] == 1) {
+				if (lasersXwing[i][0] < tieLeftSquadron[j][0] + tieWidth && lasersXwing[i][0] + 2 > tieLeftSquadron[j][0]) {
+					tieLeftSquadron[j][2] = 0;
 					lasersXwing.splice(i, 1);
 				}
 			}
@@ -282,13 +373,14 @@ function laserCol() {
 	}
 }
 
-function tLaserCol() {
-	var n = tieLasers.length;
+// Colision with middle tie lasers
+function tMidLaserCol() {
+	var n = tieMidLasers.length;
 	for (var i = 0; n > i; i++) {
-		if (tieLasers[i][1] >= xWingY && tieLasers[i][1] <= xWingY + xWingHeight) {
-			if (tieLasers[i][0] > xWingX && tieLasers[i][0] < xWingX + xWingWidth) {
+		if (tieMidLasers[i][1] >= xWingY && tieMidLasers[i][1] <= xWingY + xWingHeight) {
+			if (tieMidLasers[i][0] > xWingX && tieMidLasers[i][0] < xWingX + xWingWidth) {
 				if (nbrOfLifes > 1) {
-					tieLasers.splice(i, 1);
+					tieMidLasers.splice(i, 1);
 					nbrOfLifes -= 1;
 				} else {
 					alert("You lose.");
@@ -298,6 +390,26 @@ function tLaserCol() {
 		}
 	}
 }
+
+// Colision with left tie lasers
+function tLeftLaserCol() {
+	var n = tieLeftLasers.length;
+	for (var i = 0; n > i; i++) {
+		if (tieLeftLasers[i][1] >= xWingY && tieLeftLasers[i][1] <= xWingY + xWingHeight) {
+			if (tieLeftLasers[i][0] > xWingX && tieLeftLasers[i][0] < xWingX + xWingWidth) {
+				if (nbrOfLifes > 1) {
+					tieLeftLasers.splice(i, 1);
+					nbrOfLifes -= 1;
+				} else {
+					alert("You lose.");
+					document.location.reload();
+				}
+			}
+		}
+	}
+}
+
+
 function moveXwing() {
 	if(rightPressed && canvas.width - xWingWidth > xWingX) {
 		xWingX += 3;
@@ -313,11 +425,23 @@ function moveXwing() {
 	}
 }
 
-function xWingCol() {
-	var tie = tieSquadron.length;
+function xWingMidCol() {
+	var tie = tieMidSquadron.length;
 	for (var i = 0; tie > i; i++) {
-		if (xWingY <= tieSquadron[i][1] + tieHeight && tieSquadron[i][2] == 1) {
-			if (xWingX < tieSquadron[i][0] + tieWidth && xWingX + xWingWidth > tieSquadron[i][0]) {
+		if (xWingY <= tieMidSquadron[i][1] + tieHeight && xWingY >= tieMidSquadron[i][1] && tieMidSquadron[i][2] == 1) {
+			if (xWingX < tieMidSquadron[i][0] + tieWidth && xWingX + xWingWidth > tieMidSquadron[i][0]) {
+				alert("You crashed into the Tie Fighter!");
+				document.location.reload();
+			}
+		}
+	}
+}
+
+function xWingLeftCol() {
+	var tie = tieLeftSquadron.length;
+	for (var i = 0; tie > i; i++) {
+		if (xWingY <= tieLeftSquadron[i][1] + tieHeight && xWingY >= tieLeftSquadron[i][1] && tieLeftSquadron[i][2] == 1) {
+			if (xWingX < tieLeftSquadron[i][0] + tieWidth && xWingX + xWingWidth > tieLeftSquadron[i][0]) {
 				alert("You crashed into the Tie Fighter!");
 				document.location.reload();
 			}
@@ -334,22 +458,35 @@ function draw() {
 	addBg();
 	drawBg();
 	moveBg();
-	xWingCol();
+// XWing colision with ties	
+	xWingMidCol();
+	xWingLeftCol();
 	drawXwing();
-	addTie();
-	drawTie();
-	addTLaser();
-	moveTLaser();
-	drawTLaser();
-	tLaserCol();
+// Left Tie functions 
+	addLeftTie();
+	drawLeftTie();
+	addLeftTLaser();
+	moveLeftTLaser();
+	drawLeftTLaser();
+// Mid Tie functions
+	//addMidTie();
+	//drawMidTie();
+	//addMidTLaser();
+	//moveMidTLaser();
+	//drawMidTLaser();
+// XWing's colistion with tie lasers
+	tMidLaserCol();
+	tLeftLaserCol();
 	moveXwing();
 	pressLaser();
 	moveLaser();
 	drawLaser();
-	laserCol();
+	laserMidCol();
+	laserLeftCol();
 	shotCD += 1;	// Cooldown reset
-	shotTCD += 1;	// Cooldown on tie shots
+	shotMidTCD += 1;	// Cooldown on tie shots
+	shotLeftTCD += 1; // Cooldown on left tie shots
 }
 
-
+// vnjksbv
 setInterval(draw, 15);
